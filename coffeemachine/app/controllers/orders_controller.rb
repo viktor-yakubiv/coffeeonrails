3b4be8a8b2d68c6cecc6f6e907121a9033790758
proggeo <mashtaliryurko@gmail.com>
2016-12-03 23:49:58 +0200

    fix order errors

--- coffeemachine/app/controllers/orders_controller.rb
+++ coffeemachine/app/controllers/orders_controller.rb
@@ -28,17 +28,17 @@ class OrdersController < ApplicationController
     @order = Order.new(order_params)
     @order.user = current_user
     @order.order_sum = @order.drink.price
-    if drink.amount_left < 1
-      redirect_to root_path, notice: 'We\'re out of this drink.'
+    if @order.drink.amount_left < 1
+      return redirect_to root_path, notice: 'We\'re out of this drink.'
     end
     @order.ingredients.each do |ingredient|
       @order.order_sum += ingredient.price
       if ingredient.amount_left < 1
-        redirect_to root_path, notice: 'We\'re out of this ingredient.'
+        return redirect_to root_path, notice: 'We\'re out of this ingredient.'
       end
     end
-    if user.money_left < @order.order_sum
-      redirect_to root_path, notice: 'You do not have sufficient funds in your account'
+    if @order.user.money_left < @order.order_sum
+      return redirect_to root_path, notice: 'You do not have sufficient funds in your account'
     end
 
     respond_to do |format|
